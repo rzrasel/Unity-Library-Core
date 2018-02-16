e104394af6c893ecd1d94ff2f56fb025fabde65e
Rz Rasel <Rz Rasel>
2018-02-16 21:32:28 +0600

    Unity Mine Library Core V-201802.00.1 - 2018-02-16 21:32:27.98

---
+++ UnityMineLibraryCore/WritClassInspector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WritClassInspector : MonoBehaviour
+{
+    public int testVar1;
+    public int testVar2;
+}
