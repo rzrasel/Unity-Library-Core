61569bc8683bad1e1505789ea295a09dd720c0c7
Rz Rasel <Rz Rasel>
2018-05-14 21:37:51 +0600

    ScEmailSystem added - 2018-05-14 21:37:51.12

---
+++ UnityMineLibraryCore/ScEmailSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScEmailSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //sendEMailToSingleRecipient();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    public void sendEMailToSingleRecipient()
+    {
+        string email_string = "argrasel@gmail.com";
+        string email = email_string;
+        string subject_string = "Your subject text";
+        //email body
+        string body_string = "Your body text";
+        string subject = WWW.EscapeURL(subject_string);
+        string body = WWW.EscapeURL(body_string);
+        //Open the native default app
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+
+    }
+}
